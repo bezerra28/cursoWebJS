a2307814d9573eab39429c2acf33ecc4f241e278
alexandre <alexandrealvesbezerraa@gmail.com>
2024-04-19 20:15:52 -0300

    codigo funcioinando com while

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-//função que exibe o som
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+//função que exibe o som e recebe o id 
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 
 }
 
-//cria aqui uma lista de teclas se referenciando a elas que esta no index.html
+//cria aqui uma lista de teclas se referenciando a elas que esta no index.html na classe tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //contador do loop
@@ -13,10 +13,19 @@ let contador = 0;
 //loop
 while (contador < listaDeTeclas.length){
 
-    listaDeTeclas[contador].onclick = function(){
+        //pego a tecla clicada usando o contado do loop
+        const tecla = listaDeTeclas[contador];
+        //aqui consigo pega o parametro que indica o indice do som que foi clicado
+        const instrumento = tecla.classList[1];
 
+        //aqui usa o template string e usa no parametro da função tocaSom
+        const idAudio = `#som_${instrumento}`;
+
+        tecla.onclick = function () {
+            tocaSom(idAudio);
+        }
+    
+        contador = contador + 1;
+
+        console.log(contador);
     }
-    contador = contador + 1;
-
-    console.log(contador);
-}
