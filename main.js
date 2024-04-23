d8b24da8ba7db0fa4217b3ebd22a5a354f6ad578
alexandre <alexandrealvesbezerraa@gmail.com>
2024-04-23 08:52:30 -0300

    testado o tartamento de erro caso a função na receba seus parametros certos dia 23/04

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 //função que exibe o som e recebe o id 
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    // trata os error se a condição de tocaSom nao for correta
+    if (elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+    }else{
+        console.log('elemento nao encontrado eou seletor invalido');
+    }
 
 }
 
@@ -21,6 +28,18 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++){
         tecla.onclick = function () {
             tocaSom(idAudio);
         }
+        //evento de tecla pressionada e verifica qual foi a tecla
+        tecla.onkeydown = function(evento){
+            //verifica se a tecla clicado foi o espaço ou o enter
+            if (evento.code === 'Space' || evento.code === 'Enter'){
+                tecla.classList.add('ativa');//deixa a tecla vermelha de o espaço for clicado
+            }
+        }
+
+        tecla.onkeyup = function(){
+            tecla.classList.remove('ativa');
+        }
+        
 
        // console.log(contador);
     }
